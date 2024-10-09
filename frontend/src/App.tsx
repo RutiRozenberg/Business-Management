0955508a557cf3a302b74096ba1e8ee86fa3ee40
RutiRozenberg <ruti7633@gmail.com>
2024-10-09 19:29:50 +0300

    Arrange project architecture - add utils folder.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,36 +4,14 @@ import Home from './components/home.component'
 import Services from './components/services.component'
 import Signin from './components/signin.component'
 import ResponsiveAppBar from './components/responsiveAppBar'
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {ThemeProvider } from '@mui/material/styles';
 import Meetings from './components/meetings.component'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
+import { theme } from './utils/style/themeObject'
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#ed9ba7',
-      light: '#fbd4d5',
-      dark: '#ef8295',
-    },
-    secondary: {
-      main: '#54a1ac',
-      light: '#b5d5d6',
-      dark: '#395e73',
-    },
-    text: {
-      primary: '#54a1ac',
-      secondary: '#ed9ba7',
-    },
-  },
-  typography: {
-    fontFamily: 'Roboto, sans-serif',
-    h1: {
-      fontSize: '2.5rem',
-      fontWeight: 500,
-    },
-  },
-});
+
+
 function App() {
 
   return (
