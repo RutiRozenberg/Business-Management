5bcb35963418586d8b6e0814c5953afabc749270
RutiRozenberg <ruti7633@gmail.com>
2025-05-19 13:04:09 +0300

    Backend - Fixed a bug in the authentication message.

--- backend/src/middlewares/authentication.middleware.ts
+++ backend/src/middlewares/authentication.middleware.ts
@@ -15,26 +15,28 @@ const verifyToken = (token: string | undefined) => {
 
 
 const authentication = (req: AuthRequest, res: Response, next: NextFunction) => {
-    const authHeader = req.headers['authorization'];
-
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        throw new Error("Missing or invalid authorization header");
-    }
-    const token = authHeader.split(' ')[1];
-
     try {
+        const authHeader = req.headers['authorization'];
+
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            throw new Error("Missing or invalid authorization header");
+        }
+
+        const token = authHeader.split(' ')[1];
         req.user = verifyToken(token);
+
         next();
     } catch (err) {
         res.status(401).send("Unauthorized: Invalid Token");
+        return;
     }
 };
 
 
-const socketAuthentication = (socket: AuthSocket, next: SocketNextFunction) => {    
+const socketAuthentication = (socket: AuthSocket, next: SocketNextFunction) => {
     const token = socket.handshake.auth.token;
     try {
-        socket.user  = verifyToken(token);
+        socket.user = verifyToken(token);
         next();
     } catch (err) {
         next(new Error("Unauthorized: Invalid Token"));
